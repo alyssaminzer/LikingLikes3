c016973385e0b0a240f88f90af4288526a4b26b6
alyssaminzer <alyssa@live.ru>
2022-07-01 20:45:02 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var neil = document.querySelector('#likeNum1').innerHTML
+function add1(){
+    neil++
+    document.querySelector('#likeNum1').innerHTML = neil
+}
+
+var nichole = document.querySelector("#likeNum2").innerHTML;
+function add2() {
+    nichole++;
+    document.querySelector("#likeNum2").innerText = nichole;
+}
+
+var jim = document.querySelector('#likeNum3').innerHTML
+function add3(){
+    jim++
+    document.querySelector('#likeNum3').innerHTML = jim
+}
+
+
